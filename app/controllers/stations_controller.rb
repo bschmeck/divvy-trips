6973416be9be7c033fbf087075704044de3e32b5
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2015-03-09 06:12:30 -0500

    Just slam arrivals and departures into a table.

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,9 +1,13 @@
 class StationsController < ApplicationController
   def show
-    Station.find(params[:id])
+    @station = Station.find(params[:id])
   end
 
   def flow
-    station = Station.find(params[:id])
+    @station = Station.find(params[:id])
+    @departures = Hash.new(0)
+    @arrivals = Hash.new(0)
+    @station.departures.each{|trip| @departures[trip.departed_at.strftime("%H:%M")] += 1 }
+    @station.arrivals.each{|trip| @arrivals[trip.arrived_at.strftime("%H:%M")] += 1 }
   end
 end
