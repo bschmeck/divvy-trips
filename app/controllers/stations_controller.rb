d10188c35b359439cef15f691071ab92a4d0acb9
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2015-03-09 19:07:05 -0500

    Reduce load on DB by only plucking arrival/departure dates.

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -7,7 +7,7 @@ class StationsController < ApplicationController
     @station = Station.find(params[:id])
     @departures = Hash.new(0)
     @arrivals = Hash.new(0)
-    @station.departures.each{|trip| @departures[trip.departed_at.strftime("%H:%M")] += 1 }
-    @station.arrivals.each{|trip| @arrivals[trip.arrived_at.strftime("%H:%M")] += 1 }
+    @station.departures.pluck(:departed_at).each{|t| @departures[t.strftime("%H:%M")] += 1 }
+    @station.arrivals.pluck(:arrived_at).each{|t| @arrivals[t.strftime("%H:%M")] += 1 }
   end
 end
